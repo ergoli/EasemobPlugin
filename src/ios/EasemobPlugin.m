86da827512ebeb1f2868f8db695e8750a6e847f2
zsly <zsly3n@163.com>
2016-03-04 10:55:37 +0800

    add

--- src/ios/EasemobPlugin.m
+++ src/ios/EasemobPlugin.m
@@ -49,7 +49,7 @@
     NSDictionary *userInfo=command.arguments[1];
     NSString *groupName=command.arguments[2];
     NSNumber *serverID=command.arguments[3];
-    [self CreateChatVC:group_ID conversationType:eConversationTypeGroupChat userInfo:userInfo title:groupName server_id:[serverID integerValue]];
+    [self CreateChatVC:group_ID conversationType:eConversationTypeGroupChat userInfo:userInfo title:groupName server_id:[serverID integerValue] callbackId:command.callbackId];
 }
 
 -(void)ignoreGroupPush:(CDVInvokedUrlCommand*)command
@@ -59,7 +59,7 @@
    [[EaseMob sharedInstance].chatManager asyncIgnoreGroupPushNotification:group_ID isIgnore:[is_ignore boolValue]];
 }
 
--(void)CreateChatVC:(NSString *)conversationChatter conversationType:(EMConversationType)conversationType userInfo:(NSDictionary*)userInfo title:(NSString*)title server_id:(NSInteger)server_id
+-(void)CreateChatVC:(NSString *)conversationChatter conversationType:(EMConversationType)conversationType userInfo:(NSDictionary*)userInfo title:(NSString*)title server_id:(NSInteger)server_id callbackId:(NSString*)callbackId
 {
     AppDelegate *app_delegate=(AppDelegate*)[UIApplication sharedApplication].delegate;
     app_delegate.accessibilityValue=conversationChatter;
@@ -69,7 +69,11 @@
     chat.userInfo=userInfo;
     chat.server_id=server_id;
     UINavigationController *nav=[[UINavigationController alloc] initWithRootViewController:chat];
-    [app_delegate.viewController presentViewController:nav animated:YES completion:nil];
+    [app_delegate.viewController presentViewController:nav animated:YES completion:^{
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }];
+    
 }
 
 /*应用内接收环信消息*/
